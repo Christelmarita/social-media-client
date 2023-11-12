c1c24f9c55cb2485cfe231ba36d8974bdce5f9f4
Christelmarita <christelmarita.oe@gmail.com>
2023-11-12 17:41:04 +0100

    package and cypress.config changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   env: {
     pass_email: 'jaweel@noroff.no',
     pass_password: 'password',
+    fail_email: 'jaweel@error.no',
+    fail_password: 'notmypassword',
   },
   e2e: {
     setupNodeEvents(on, config) {},
